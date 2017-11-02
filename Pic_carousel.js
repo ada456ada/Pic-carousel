8ae58ad55e33f6a2658048a438127d9e580715ba
LAW <632961103@qq.com>
2017-11-02 19:39:35 +0800

    vue重构

--- Pic_carousel.js
+++ Pic_carousel.js
@@ -1,56 +1,93 @@
-window.onload=function(){
-	var oP=document.getElementById('p1');
-	var oSpan=document.getElementById('span1');
-	var oNext=document.getElementById('next');
-	var oPrev=document.getElementById('prev');
-	var oImg=document.getElementById('img1');
-	var oTrue=document.getElementById('true');
-	var oP2=document.getElementById('p2');
-	var oFalse=document.getElementById('false');
-	var oP3=document.getElementById('p3');
-	var arr1=["西厢记","前目的地","搏击俱乐部","记忆碎片","京东"];
-	var arr2=["pic/0.jpg","pic/1.jpg","pic/2.jpg","pic/3.jpg","pic/jd.jpg"];
-	var i=0;
-	function kk(){
-		oP.innerHTML=i+1+"/"+arr1.length;
-		oSpan.innerHTML=arr1[i];
-		oImg.src=arr2[i];
-	}
-	kk();//初始化
-	 	oTrue.onclick=function(){
-	 		oP2.style.display="block";
-	 		oP3.style.display="none";
-			oNext.onclick=function(){
-				i++;
-				if(i==arr1.length)
+$(function(){
+	let oP=$('#p1')[0],
+ 		oSpan=$('#span1')[0],
+ 		oNext=$('#next')[0],
+ 		oPrev=$('#prev')[0],
+ 		oImg=$('#img1')[0],
+ 		oP2=$('#p2')[0],
+ 		aA=$('#aA')[0],
+		arr1=["西厢记","前目的地","搏击俱乐部","记忆碎片","京东"],
+		arr2=["pic/0.jpg","pic/1.jpg","pic/2.jpg","pic/3.jpg","pic/jd.jpg"],
+		oDiv2=$('#div2')[0],
+		i=0,
+		vm=new Vue({
+			el:'#div2',
+			data:{
+				message:'循环播放'
+			}
+		}),
+		chioce1={
+			oNext(){
+				if(++i==arr1.length){
 					i=0;
-				kk();
 				}
-			oPrev.onclick=function(){
-				i--;
-				if (i==-1)
+				kk();
+				},
+			oPrev(){
+				if (--i==-1){
 					i=arr1.length-1;
+				}
 				kk();
 				}
-	 	}
-	 	oFalse.onclick=function(){
-	 		oP3.style.display="block";
-	 		oP2.style.display="none";
-	 		oNext.onclick=function(){
-				i++;
-				if(i==arr1.length){
+		},
+		chioce2={
+			oNext(){
+				if(++i==arr1.length){
 					alert("这已经是最后一页了");
 					i--;
 				}
 				kk();
-				}
-			oPrev.onclick=function(){
-				i--
-				if (i==-1){
+				},
+			oPrev(){
+				if (--i==-1){
 					alert("已经是第一页了，不能再往前了");
 					i++;
-				}
+				}	
 				kk();
 				}
-	 	}
-}
+		},
+		kk=()=>{
+			oP.innerHTML=i+1;
+			oP.innerHTML+="/";
+			oP.innerHTML+=arr1.length;
+			oSpan.innerHTML=arr1[i];
+			oImg.src=arr2[i];
+		};
+	kk();//初始化
+	oDiv2.addEventListener('click',function(e){
+		vm.message=(e.target.value=='循环播放')?'循环播放':'顺序播放';
+	});
+	aA.addEventListener('click',function(e){
+		let word=(vm.message=='循环播放')?true:false,
+			aoch=(e.target.id==='next')?true:false;
+		if(word){
+			if(aoch){
+				chioce1.oNext();
+			}else{
+				chioce1.oPrev();
+			}			
+		}else{
+			if(aoch){
+				chioce2.oNext();
+			}else{
+				chioce2.oPrev();
+			}
+		}
+		});	
+	$(document).on('keyup',function(e){
+		let keyCode = e.keyCode;
+		if(keyCode == 37){ // 上一篇
+			if(++i==arr1.length){
+				i=0;
+			}
+			kk();
+		}
+		if(keyCode == 39){ // 下一篇
+			if (--i==-1){
+				i=arr1.length-1;
+			}
+			kk();
+		}
+	});
+});
+	
